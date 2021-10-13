49682e44430c3821ffa7695263cca4b89e17671e
Md Mainul Hoque <minulhira@Mds-MacBook-Pro.local>
2021-10-14 02:04:07 +0600

    added try except in task

--- plans/tasks.py
+++ plans/tasks.py
@@ -3,19 +3,23 @@ from datetime import date
 
 
 def deduct_money(phone, amount, daily_process=False):
-    if daily_process:
-        customer_plans = CustomerPlan.objects.filter(is_active=True)
-        for customer_plan in customer_plans:
-            if customer_plan.end_date == date.today():
-                customer_plan.is_active=False
-                customer_plan.save()
-            else:
-                date_diff = date.today() - customer_plan.start_date
-                print(date_diff.days)
-                if date_diff.days % 30 == 0:
-                    deduct_money_from_gateway(customer_plan.customer.phone, customer_plan.amount)
-    else:
-        deduct_money_from_gateway(phone, amount)
+    try:
+        if daily_process:
+            customer_plans = CustomerPlan.objects.filter(is_active=True)
+            for customer_plan in customer_plans:
+                if customer_plan.end_date == date.today():
+                    customer_plan.is_active=False
+                    customer_plan.save()
+                else:
+                    date_diff = date.today() - customer_plan.start_date
+                    print(date_diff.days)
+                    if date_diff.days % 30 == 0:
+                        deduct_money_from_gateway(customer_plan.customer.phone, customer_plan.amount)
+        else:
+            deduct_money_from_gateway(phone, amount)
+    except Exception as e:
+        print('Task failed')
+        print(e)
 
 
 
